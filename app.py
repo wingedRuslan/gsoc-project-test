262f011c6f73a99c4e62d202e033a6fe29e32daa
Ruslan Yermakov <yermakovruslan@gmail.com>
2020-03-23 01:12:13 +0100

    Create app to return posts by way of a query string in the URL, e.g. ordered by view count or score. (default-chronological order)

--- app.py
+++ app.py
@@ -13,14 +13,23 @@ def index():
     # Define names of database, table and the column to order rows by
     DB_NAME = "bioposts.db"
     TABLE_NAME = "bioinformatics_posts"
-    ORDER_COL = "CreationDate"
 
-    # Query the DB to get all the posts in chronological order
-    records = readSqliteTable(db_name=DB_NAME, table_name=TABLE_NAME, order_col=ORDER_COL)
+    # Get the "sorting" URL argument
+    sorting_column = request.args.get("sorting")
 
     # Get the columns names from the XML file
     pathToFile = "./dataset/bioinformatics_posts_se.xml"
     list_columns, _ = getColumnsFromXML(pathToFile)
+
+    # Sort rows based on the input argument "sorting" including "Score" & "ViewCount"
+    if sorting_column in list_columns:
+        ORDER_COL = sorting_column
+    else:
+        # Default sorting value - chronological order
+        ORDER_COL = "CreationDate"
+
+    # Query the DB to get all the posts in the specified order
+    records = readSqliteTable(db_name=DB_NAME, table_name=TABLE_NAME, order_col=ORDER_COL)
 
     # render the template with the columns and data
     return render_template("tables.html", list_columns=list_columns, records=records)
